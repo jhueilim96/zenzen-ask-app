7333e529458cdb9392b5b3b4dccbde7ba742147b
Lim Jian Huei <jhueilim96@gmail.com>
2024-01-28 22:40:15 +0800

    refactor data binding and complete serp and kp

--- fe/prototype-ui.py
+++ fe/prototype-ui.py
@@ -5,19 +5,19 @@ from typing import Any
 from jinja2 import Template
 
 # Model
-class Hit(BaseModel):
-    document: dict[str, Any]
-    text_match: int
-    highlights: list[dict[str, Any]]
+# class Hit(BaseModel):
+#     document: dict[str, Any]
+#     text_match: int
+#     highlights: list[dict[str, Any]]
 
-class TypesenseSearchResult(BaseModel):
-    found: int
-    hits: list[Hit]
-    page: int
-    search_time_ms: int
+# class TypesenseSearchResult(BaseModel):
+#     found: int
+#     hits: list[Hit]
+#     page: int
+#     search_time_ms: int
 
-class TypesenseMultiSearchResult(BaseModel):
-    results: list[TypesenseSearchResult]
+# class TypesenseMultiSearchResult(BaseModel):
+#     results: list[TypesenseSearchResult]
 
 class KnowledgePanel(BaseModel):
     name: str
@@ -32,19 +32,29 @@ class SearchResultRow(BaseModel):
     url: str
     description: str = "Lorem Ipsum"
 
+class SearchResultPage(BaseModel):
+    result: list[SearchResultRow]
+    found: int
+    page: int
+    search_time_ms: int
+
+class SearchResponseConsolidated(BaseModel):
+    serp: SearchResultPage
+    kp: KnowledgePanel
+
 # Function
 @st.cache_data
-def search(query:str):
+def search(query:str)->SearchResponseConsolidated:
     URL = 'http://127.0.0.1:8000/search'
     r = httpx.get(URL, params={'q':query})
-    multi_search_result = TypesenseMultiSearchResult.model_validate(r.json())
-    return multi_search_result.results[0], multi_search_result.results[1]
+    multi_search_result = SearchResponseConsolidated.model_validate(r.json())
+    return multi_search_result
 
 
 def create_serp(serp_data:list[SearchResultRow], container):
     for search_result_row in serp_data:
-        container.markdown(f"""### [{search_result_row.title}]({search_result_row.url})
-[{search_result_row.url}]({search_result_row.url})
+        container.markdown(f"""##### [{search_result_row.title}]({search_result_row.url})
+{search_result_row.url}
 
 {search_result_row.description}
 """)
@@ -57,6 +67,7 @@ def create_knowledge_panel(entity:KnowledgePanel, container):
 ## About
 {{entity.description}}
 
+
 [Source]({{entity.source}})
 
 {% for key, val in entity.properties.items() %}
@@ -64,40 +75,44 @@ def create_knowledge_panel(entity:KnowledgePanel, container):
 {% endfor %}
 """)
 
-    container.image(entity.image_url)
-    container.markdown(kp_template.render({'entity':entity.model_dump()}))
+    if entity.image_url:
+        container.image(entity.image_url)
+    container.markdown(kp_template.render({'entity':entity.model_dump()}), unsafe_allow_html=True)
 
 
 # Main
 st.title('Zenzen')
-query = st.text_input("Search something")
+query = st.text_input("Search something", value="naruto movie")
 
-serp_col, kp_col = st.columns([2, 1])
+serp_col, kp_col = st.columns([1, 1])
 
 # Search Result Page Processing
-webpage_result, kp_result = search(query)
-search_result_list = [
-    SearchResultRow(
-        title= hit.document.get('title'),
-        url=hit.document.get('url'),
-    )
-    for hit in webpage_result.hits
-]
+search_response = search(query)
+webpage_result = search_response.serp
+kp_data = search_response.kp
+
+# search_result_list = [
+#     SearchResultRow(
+#         title= hit.document.get('title'),
+#         url=hit.document.get('url'),
+#     )
+#     for hit in webpage_result.hits
+# ]
 
 # Knowledge Panel Process
-kg_entity = kp_result.hits[0].document.copy()
-kp_data = KnowledgePanel(
-    name = kg_entity.pop('name') ,
-    type = kg_entity.pop('type') ,
-    description = kg_entity.pop('description')[:300]+'...' if 'description' in kg_entity.keys() else '',
-    image_url =  kg_entity.pop('image_url') if 'image_url' in kg_entity.keys() else '',
-    source = kg_entity.pop('url') if 'url' in kg_entity.keys() else '',
-    properties =  kg_entity
-)
+# kg_entity = kp_result.hits[0].document.copy()
+# kp_data = KnowledgePanel(
+#     name = kg_entity.pop('name') ,
+#     type = kg_entity.pop('type') ,
+#     description = kg_entity.pop('description')[:300]+'...' if 'description' in kg_entity.keys() else '',
+#     image_url =  kg_entity.pop('image_url') if 'image_url' in kg_entity.keys() else '',
+#     source = kg_entity.pop('url') if 'url' in kg_entity.keys() else '',
+#     properties =  kg_entity
+# )
 
 # Search Result View
 serp_col.text(f"{webpage_result.found} results is found in {webpage_result.search_time_ms} millisecons")
-create_serp(search_result_list, serp_col)
+create_serp(webpage_result.result, serp_col)
 
 # Knowledge Panel View
 kp_col.text("Knowledge Panel")
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from typing import Any
+import asyncio
 
 
 class Hit(BaseModel):
@@ -19,6 +20,29 @@ class TypesenseSearchResult(BaseModel):
 
 class TypesenseMultiSearchResult(BaseModel):
     results: list[TypesenseSearchResult]
+
+class KnowledgePanel(BaseModel):
+    name: str
+    type: list[str]
+    description: str = ''
+    image_url: str = None
+    source: str = None
+    properties: dict[str, Any] = {}
+
+class SearchResultRow(BaseModel):
+    title: str
+    url: str
+    description: str = "Lorem Ipsum"
+
+class SearchResultPage(BaseModel):
+    result: list[SearchResultRow]
+    found: int
+    page: int
+    search_time_ms: int
+
+class SearchResponseConsolidated(BaseModel):
+    serp: SearchResultPage
+    kp: KnowledgePanel
 
 
 load_dotenv()
@@ -39,18 +63,18 @@ app = FastAPI()
 
 
 @app.get("/search")
-async def search(q:str, limit:int=10, ):
+async def search(q:str, limit:int=10, )->SearchResponseConsolidated:
     search_requests = {
         'searches': [
         {
             'collection': 'webpage',
             'q': q,
-            'query_by': 'title, url, aka',
+            'query_by': 'title, url, aka, entity_type',
         },
         {
             'collection': 'entity',
             'q': q,
-            'query_by': 'name, type, description',
+            'query_by': 'name, type, synonyms',
         }
         ]
     }
@@ -61,4 +85,54 @@ async def search(q:str, limit:int=10, ):
     }
 
     search_hits = client.multi_search.perform(search_requests, common_search_params)
-    return TypesenseMultiSearchResult.model_validate(search_hits)
+    multi_search_result = TypesenseMultiSearchResult.model_validate(search_hits)
+    webpage_result, kp_result = multi_search_result.results[0], multi_search_result.results[1]
+
+    # Webpage Search Result Processing
+    search_result_list = [
+        SearchResultRow(
+            title= hit.document.get('title'),
+            url=hit.document.get('url'),
+        )
+        for hit in webpage_result.hits
+    ]
+    serp = SearchResultPage(
+        result=search_result_list,
+        found=webpage_result.found,
+        page=webpage_result.page,
+        search_time_ms=webpage_result.search_time_ms,
+    )
+
+    # Knowledge Panel Result Processing
+    kg_entity = kp_result.hits[0].document.copy()
+    for highlight in  kp_result.hits[0].highlights:
+        highlight_field = highlight['field']
+        content_to_highlight = kg_entity[highlight_field]
+
+        if isinstance(content_to_highlight, str):
+            for token in highlight['matched_tokens']:
+                content_to_highlight = content_to_highlight.replace(token, f"<mark>{token}</mark>", 1)
+            kg_entity[highlight_field] = content_to_highlight
+        elif isinstance(content_to_highlight, list):
+            pass
+        pass
+
+    kp_name = kg_entity.pop('name')
+    kp_type = kg_entity.pop('type')
+    kp_description = kg_entity.pop('description') if 'description' in kg_entity.keys() else ''
+    kp_description = kp_description[:500] + '...' if len(kp_description) > 500 else kp_description
+    kp_image_url =  kg_entity.pop('image_url') if 'image_url' in kg_entity.keys() else ''
+    kp_source = kg_entity.pop('url') if 'url' in kg_entity.keys() else ''
+    kp_properties =  kg_entity
+    kp_data = KnowledgePanel(
+        name=kp_name
+        , type=kp_type
+        , description=kp_description
+        , image_url=kp_image_url
+        , source=kp_source
+        , properties=kp_properties
+    )
+    return SearchResponseConsolidated(serp=serp, kp=kp_data)
+
+
+# asyncio.run(search('Naruto movie'))
